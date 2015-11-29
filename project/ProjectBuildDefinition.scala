50c5580afd2f3d04a7a9e8d835840db121317ca5
Kevin Mas Ruiz <masruizkevin@gmail.com>
2015-11-29 14:07:51 +0100

    play project dependencies

--- project/ProjectBuildDefinition.scala
+++ project/ProjectBuildDefinition.scala
@@ -4,7 +4,11 @@ import Keys._
 object ProjectBuildDefinition extends Build {
   lazy val ScalaTest = "org.scalatest" %% "scalatest" % "2.0" % "test"
   lazy val NScalaTime = "com.github.nscala-time" %% "nscala-time" % "2.6.0"
+  def playProject(project: Project) = project settings(
+      resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      libraryDependencies ++= Seq("com.typesafe.play" %% "play" % "2.2.2")
+    )
 
-  lazy val api = project dependsOn core
+  lazy val api = playProject(project) dependsOn core
   lazy val core = project settings(libraryDependencies ++= Seq(ScalaTest, NScalaTime))
 }
