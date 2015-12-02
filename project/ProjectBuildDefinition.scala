d60a00b79b95ab97f1694593ae096b2f809870d0
Kevin Mas Ruiz <masruizkevin@gmail.com>
2015-12-02 18:33:54 +0100

    cli example

--- project/ProjectBuildDefinition.scala
+++ project/ProjectBuildDefinition.scala
@@ -5,7 +5,9 @@ import play.sbt._
 object ProjectBuildDefinition extends Build {
   lazy val ScalaTest = "org.scalatest" %% "scalatest" % "2.0" % "test"
   lazy val NScalaTime = "com.github.nscala-time" %% "nscala-time" % "2.6.0"
+  lazy val Scallop = "org.rogach" %% "scallop" % "0.9.5"
 
-  lazy val api = project.enablePlugins(PlayScala) dependsOn core
+  lazy val api = project enablePlugins PlayScala dependsOn core
+  lazy val cli = project settings(libraryDependencies ++= Seq(Scallop)) dependsOn core
   lazy val core = project settings(libraryDependencies ++= Seq(ScalaTest, NScalaTime))
 }
